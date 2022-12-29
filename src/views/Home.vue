2d42a057cd875148a2e792e8f9530ae908e8e84d
nurdin najib <najibnurdin999@gmail.com>
2022-12-29 19:21:49 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <section class="max-w-sm w-full mx-auto h-full py-12 bg-gray-100">
+        <div class="flex flex-col text-center justify-center space-y-7">
+            <h4 class="text-indigo-900 text-lg font-medium">Muhammad yusuf Azmi Najib</h4>
+            <h1 class="text-indigo-900 text-3xl font-bold">Syukuran Khitanan</h1>
+            
+            <div class="flex justify-center">
+                <img class="w-20 h-40" src="https://media.istockphoto.com/id/485456676/id/foto/lentera-vintage-maroko-terisolasi-dengan-latar-belakang-putih.jpg?s=612x612&w=0&k=20&c=uc5NY294d44qVEyrAD8MNuQdVN01vhWZntTEY4Zzplc=" />
+            </div>
+            <h2 class="text-indigo-900 text-xl font-bold">Sabtu, 21 Januari 2023</h2>
+            <p class="text-indigo-900 text-sm font-small">untuk </p>
+            <p class="text-indigo-900 text-xl font-bold">Budi dan Ani</p>
+            <p class="text-indigo-900 text-sm font-small">di Jakarta </p>
+            <div class="flex justify-center">
+                <button class="bg-red-400 p-2 rounded text-gray-100">Buka undangan </button>
+            </div>
+        </div>
+    </section>
+</template>
