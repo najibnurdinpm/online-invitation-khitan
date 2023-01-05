948282db5cdb84785786a1e78de1cd7f487f7047
nurdin najib <najibnurdin999@gmail.com>
2023-01-05 22:31:26 +0700

    revisi hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,22 +21,9 @@
 
   <section class="bg-[url('@/assets/bg-hero.jpg')] relative max-w-sm w-full mx-auto py-12 overflow-hidden">
     <div class="flex flex-1 h-full ">
-      <div class="w-3/4 bg-yellow-900 px-10 pt-10 pb-80">
-        <p class="text-yellow-300 font-small text-2xl leading-[40px]">Undangan</p>
-        <h2 class="text-yellow-300 font-bold text-5xl">Khitanan</h2>
-        <p class="text-yellow-300 font-small text-sm leading-[40px]">Sabtu, 21 Januari 2023</p>
-        <div class="mt-12 max-w-[90%]">
-          <h3 class="text-yellow-300 font-small text-sm">M. Yusuf Azmi Najib</h3>
-          <p class="text-center text-yellow-300 font-small text-sm">(Yusuf)</p>
-        </div>
-      </div>
-    </div>
-    <div id="box-image-hero" class=" w-3/4 absolute bottom-0 right-0 h-[450px] ">
-      <div class="relative h-full right-0 bg-[url('@/assets/hero.png')] ">
-        
-      </div>
-    </div> 
-    <div class="absolute bottom-20 w-full px-14 h-40 z-10">
+      <div class="w-3/4 bg-yellow-900 px-10 pt-10 pb-80 relative">
+      
+    <div class="absolute bottom-20 w-full px-0 h-40 z-10">
       <div id="label-name" class="text-center   h-full ">
         <p class="text-yellow-300 font-bold mb-3">Kepada Yth</p>
         <div class="w-full h-full opacity-75 bg-yellow-100 rounded flex justify-center items-center">
@@ -49,6 +36,20 @@
         </div>
       </div>
     </div>
+        <p class="text-yellow-300 font-small text-sm ">Undangan</p>
+        <h2 class="text-yellow-300 font-bold text-4xl">Khitanan</h2>
+        <p class="text-yellow-300 font-small text-sm ">Sabtu, 21 Januari 2023</p>
+        <div class="mt-12 max-w-[90%]">
+          <h3 class="text-yellow-300 font-small text-sm">M. Yusuf Azmi Najib</h3>
+          <p class="text-center text-yellow-300 font-small text-sm">(Yusuf)</p>
+        </div>
+      </div>
+    </div>
+    <div id="box-image-hero" class=" w-3/4 absolute bottom-0 right-0 h-[450px] ">
+      <div class="relative h-full right-0 bg-[url('@/assets/hero.png')] ">
+        
+      </div>
+    </div> 
   </section>
 <section class="bg-yellow-50 relative max-w-sm w-full  h-auto mx-auto py-10 px-10">
   <div class=" text-center text-yellow-900 py-5  space-y-5">
